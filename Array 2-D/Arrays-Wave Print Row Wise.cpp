4ab13f3bd28dcb1d5fb175e389147d84a8e7dd76
Ayush Katiyar <89083667+ayushkxyz@users.noreply.github.com>
2022-01-22 23:44:13 +0530

    Create Arrays-Wave Print Row Wise.cpp

---
+++ Array 2-D/Arrays-Wave Print Row Wise.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int arr[100][100];
+	int row,col;
+	cin>>row>>col;
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cin>>arr[i][j];
+		}
+	}
+	for(int i=0;i<row;i++){
+		if(i%2==0){
+			for(int j=0;j<col;j++){
+				cout<<arr[i][j]<<", ";
+			}
+		}
+		else{
+			for(int j=col-1;j>=0;j--){
+				cout<<arr[i][j]<<", ";
+			}
+		}
+	}
+	cout<<"END";
+	return 0;
+}
