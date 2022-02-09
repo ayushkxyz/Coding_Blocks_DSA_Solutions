ad2c4f37a9e8af7bd30a33beca22bec291fd520e
Ayush Katiyar <89083667+ayushkxyz@users.noreply.github.com>
2022-02-09 12:34:49 +0530

    Create Sort_string.cpp

---
+++ Character Array/Miscellaneous/Sort_string.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    cin.ignore();
+    string s[n];
+    for(int i=0;i<n;i++){
+        getline(cin,s[i]);
+    }
+    sort(s,s+n);
+    for(int i=0;i<n;i++){
+        cout<<s[i]<<endl;
+    }
+
+}
