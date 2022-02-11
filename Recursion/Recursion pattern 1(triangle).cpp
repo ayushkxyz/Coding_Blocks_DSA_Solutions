9220bbb321c955ccfa95a4fa7dcac2675273c06e
Ayush Katiyar <89083667+ayushkxyz@users.noreply.github.com>
2022-02-11 19:46:09 +0530

    Create Recursion pattern 1(triangle).cpp

---
+++ Recursion/Recursion pattern 1(triangle).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void pattern(int n){
+	if(n==0){
+		return ;
+	}
+	pattern(n-1);
+	for(int i=1;i<=n;i++){
+		cout<<"*\t";
+	}
+	cout<<endl;
+}
+int main() {
+	int n;
+	cin>>n;
+	pattern(n);
+	return 0;
+}
