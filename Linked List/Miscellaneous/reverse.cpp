1bbc5f14564e9156a5859b8b759092117e06d4e7
Ayush Katiyar <89083667+ayushkxyz@users.noreply.github.com>
2022-02-14 22:58:32 +0530

    Create reverse.cpp

---
+++ Linked List/Miscellaneous/reverse.cpp
@@ -0,0 +1,55 @@
+ #include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node*next;
+    node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+void reverse(node* &head){
+      node*prev=NULL;
+      node*curr=head;
+      node*temp;
+      while(curr!=NULL){
+          temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+         }
+         head=prev;
+
+
+}
+void insert_end(node* &head){
+       int d;
+       cin>>d;
+       node*follow;
+       while(d!=-1){
+           if(head==NULL){
+               head=new node(d);
+               follow=head;
+           }
+          else{
+            follow->next=new node(d);
+            follow=follow->next;
+          } 
+          cin>>d;
+       }
+}
+ void Print(node*head){
+     while(head!=NULL){
+         cout<<head->data<<" ";
+         head=head->next;
+     }
+}
+int main(){
+    node*head=NULL;
+     cout<<"enter the Elements \n";
+     insert_end(head);
+     reverse(head);
+     Print(head);
+}
+
